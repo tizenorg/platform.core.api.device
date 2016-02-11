bf42250b59e52301648d31903d0666b52065e681
pr.jung <pr.jung@samsung.com>
2016-02-11 16:49:19 +0900

    Implement IR apis.
    - device_ir_is_available : Check whether ir is available
    - device_ir_transmit : Send IR command
    
    Change-Id: Ib96cc19675d6d1b5cb7fc2c0a635ef5ed99bc83d
    Signed-off-by: pr.jung <pr.jung@samsung.com>

--- src/ir.c
+++ src/ir.c
@@ -15,17 +15,60 @@
  */
 
 #include <stdio.h>
-#include <stdbool.h>
 
 #include "ir.h"
+#include "common.h"
+#include "dbus.h"
+
+#define METHOD_IS_AVAILABLE		"IRIsAvailable"
+#define METHOD_TRANSMIT			"TransmitIR"
 
 int device_ir_is_available(bool *available)
 {
+	int ret;
+
+	if (!available)
+		return DEVICE_ERROR_INVALID_PARAMETER;
 	*available = false;
+
+	ret = dbus_method_sync(DEVICED_BUS_NAME, DEVICED_PATH_IR,
+			DEVICED_INTERFACE_IR, METHOD_IS_AVAILABLE,
+			NULL, NULL);
+
+	if (ret < 0)
+		return DEVICE_ERROR_OPERATION_FAILED;
+
+	*available = ret;
 	return DEVICE_ERROR_NONE;
 }
 
 int device_ir_transmit(int carrier_frequency, int *pattern, int size)
 {
+	char *arr[1];
+	struct dbus_int pattern_list;
+	int freq_pattern[size + 1];
+	int ret;
+	int i;
+
+	if (size < 0) {
+		_D("IR pattern size is negative");
+		return DEVICE_ERROR_INVALID_PARAMETER;
+	}
+
+	freq_pattern[0] = carrier_frequency;
+	for (i = 1; i <= size; i++)
+		freq_pattern[i] = pattern[i-1];
+
+	pattern_list.list = freq_pattern;
+	pattern_list.size = size + 1;
+	arr[0] = (char *)&pattern_list;
+
+	ret = dbus_method_sync(DEVICED_BUS_NAME, DEVICED_PATH_IR,
+			DEVICED_INTERFACE_IR, METHOD_TRANSMIT,
+			"ai", arr);
+
+	if (ret < 0)
+		return DEVICE_ERROR_OPERATION_FAILED;
+
 	return DEVICE_ERROR_NONE;
 }
